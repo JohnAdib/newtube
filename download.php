25d403c3d2855df8ed000e0bb60fbd9fd5fb0172
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-04-22 02:44:34 +0430

    add cli argument.

--- download.php
+++ download.php
@@ -5,21 +5,46 @@ require __DIR__ . "/vendor/autoload.php";
 use YoutubeDl\Options;
 use YoutubeDl\YoutubeDl;
 
-$yt = new YoutubeDl();
+$arguments = getopt("l:");
 
-$collection = $yt->download(
-    Options::create()
-        ->downloadPath(__DIR__ . "/media")
-        ->mergeOutputFormat("mp4")
-        ->writeThumbnail(true)
-        ->url("https://www.youtube.com/watch?v=oDAw7vW7H0c")
-);
+if (!isset($arguments["l"])) {
+    die("ERROR : there is no `-l LINK`");
+}
+if (!is_array($arguments["l"])) {
+    $arguments["l"] = [$arguments["l"]];
+}
+foreach ($arguments["l"] as $url) {
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        echo "`" . $url . "` is not a valid url \n";
+        continue;
+    }
+    echo "downloading... \n";
+    download_link($url);
+    echo "\n";
+}
+die();
 
-foreach ($collection->getVideos() as $video) {
-    if ($video->getError() !== null) {
-        echo "Error downloading video: {$video->getError()}.";
-    } else {
-        echo $video->getTitle(); // Will return Phonebloks
-        // $video->getFile(); // \SplFileInfo instance of downloaded file
+/**
+ * https://github.com/norkunas/youtube-dl-php
+ */
+function download_link(string $_link): void
+{
+    $yt = new YoutubeDl();
+
+    $collection = $yt->download(
+        Options::create()
+            ->downloadPath(__DIR__ . "/media")
+            ->mergeOutputFormat("mp4")
+            ->writeThumbnail(true)
+            ->url($_link)
+    );
+
+    foreach ($collection->getVideos() as $video) {
+        if ($video->getError() !== null) {
+            echo "Error downloading {$_link}: {$video->getError()}.";
+        } else {
+            echo "complete : " . $video->getTitle();
+            // $video->getFile();
+        }
     }
 }
