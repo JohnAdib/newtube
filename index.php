0bb118140fd18c95549d6e7b060e1b3710d7ed75
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-21 19:54:23 +0430

    add back

--- index.php
+++ index.php
@@ -18,6 +18,27 @@
 </head>
 <body class="bg-pink-50">
  <div class="container mx-auto mt-4 mb-4">
+
+  <nav class="bg-white py-2 md:py-4 mb-4">
+    <div class="container px-4 mx-auto md:flex md:items-center">
+
+      <div class="flex justify-between items-center">
+        <a href="<?php echo getUrl_wo_params(); ?>" class="font-bold text-xl text-indigo-600">PesonalTube</a>
+        <button class="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
+          <i class="fas fa-bars"></i>
+        </button>
+        <a href="https://MrAdib.com/personalTube" class="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">About</a>
+      </div>
+
+      <div class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+<?php if($_GET && $_GET["id"]) {?>
+        <a href="<?php echo getUrl_wo_params(); ?>" class="p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-solid border-indigo-600 rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Back</a>
+<?php }?>
+      </div>
+    </div>
+  </nav>
+
+
 <?php
 	if($_GET && $_GET["id"])
 	{
